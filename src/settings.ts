20fe94c516df00e09f6210aa31046d5a57092afa
Colin <colin@lenovo.com>
2022-12-02 07:53:09 +0000

    Define an outline of the plugin options

---
+++ src/settings.ts
@@ -0,0 +1,15 @@
+export interface ISettings {
+
+    loggingLevel : boolean;
+
+    renderCookware: boolean;
+    renderImages: boolean;
+    renderIngredients: boolean;
+    renderInlineTimers: boolean;
+    renderQuantitiesInline: boolean;
+    renderTimers: boolean;
+    renderTotalTime: boolean;
+    
+    timerTickSound : boolean
+    timerEndSound : boolean
+  }
